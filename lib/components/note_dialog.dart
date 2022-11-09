c197e05d9e6c3d639bf5aefc447bbe4b54d500d8
mritter <mritter@ci-database.de>
2022-11-09 02:46:07 +0100

    zrdz

--- lib/components/note_dialog.dart
+++ lib/components/note_dialog.dart
@@ -1,70 +1,88 @@
 import 'package:blutodo/data/notes_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class NoteDialog extends StatelessWidget {
   NoteDialog({
+    this.noteID,
     required this.onClose,
     required this.notesRepository,
     Key? key,
   }) : super(key: key);
 
   final VoidCallback onClose;
-
   final NotesRepository notesRepository;
+  final String? noteID;
+
+  NoteModel noteModel;
 
   final controllerTitle = TextEditingController();
   final controllerDescription = TextEditingController();
 
+  holen(){
+      notesRepository.getNote(noteID.toString());
+  }
+
   save() {
-    final model = NoteModel(
-      title: controllerTitle.text.trim(),
-      description: controllerTitle.text.trim()
-    );
+    noteModel.title = controllerTitle.text.trim();
+    noteModel.description = controllerDescription.text.trim();
 
-    if (model.title == "" && model.description == ""){
-          onClose();
-          return;
+    if (noteModel.title == "" && noteModel.description == "") {
+      onClose();
+      return;
     }
-    notesRepository.createNote(model: model);
+    notesRepository.createNote(model: noteModel);
     onClose();
-
   }
 
   @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: TextFormField(
-                  controller: controllerTitle,
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    hintText: "Aufgabe",
-                  ),
-                )),
-            Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: TextFormField(
-                  minLines: 2,
-                  maxLines: 100,
-                  controller: controllerDescription,
-                  decoration: const InputDecoration(
-                    hintText: "Beschreibung",
-                    border: UnderlineInputBorder(),
-                  ),
-                )),
-            Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: ElevatedButton.icon(
-                  onPressed: () => {save()},
-                  icon: const Icon(Icons.save),
-                  label: const Text('Speichern'),
-                ))
-          ],
-        ),
-      );
+  Future<Widget> build(BuildContext context) async   {
+    /*
+        if (noteID != null) {
+    
+            final test =  notesRepository.getNote(noteID.toString());
+     
+        }
+*/
+
+    final test = awaeit notesRepository.getNote(noteID.toString());
+
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextFormField(
+                controller: controllerTitle,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: "Aufgabe",
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextFormField(
+                minLines: 2,
+                maxLines: 100,
+                controller: controllerDescription,
+                decoration: const InputDecoration(
+                  hintText: "Beschreibung",
+                  border: UnderlineInputBorder(),
+                ),
+              )),
+          Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: ElevatedButton.icon(
+                onPressed: () => {save()},
+                icon: const Icon(Icons.save),
+                label: const Text('Speichern'),
+              ))
+        ],
+      ),
+    );
+  }
 }
